ab1b46101f55cc658d5f696369464f44725cfe38
ManuelBravaccini <manuelbravaccini2000@gmail.com>
2022-11-21 17:58:06 +0100

    Completed script

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const km = parseInt(prompt("Inserisci quanti kilometri vuoi fare"));
-const age = parseInt(prompt("Inserisci la tua età"))
-let price = km * 0.21
-console.log(price)
+const age = parseInt(prompt("Inserisci la tua età"));
+
+let price = km * 0.21;
+
+if (age < 18){
+    price = price - ((price / 100) * 20)
+} else if (age > 65){
+    price = price - ((price / 100) * 40)
+}
+
+document.getElementById('price').innerHTML = price.toFixed(2) + '&euro;';
